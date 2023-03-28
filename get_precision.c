411717c34d90f174c149cf68460125e1a11e34fe
Meslar <122877839+Meslar@users.noreply.github.com>
2023-03-28 21:31:33 +0000

    Update get_precision.c

--- get_precision.c
+++ get_precision.c
@@ -10,52 +10,32 @@
 
 int get_precision(const char *format, int *i, va_list list)
 {
-    int curr_i = *i + 1;
-    int precision = -1;
-    int state = 0;
+	int curr_i = *i + 1;
+	int precision = -1;
 
-    while (format[curr_i] != '\0')
-    {
-        switch (state)
-        {
-            case 0:  // initial state
-                if (format[curr_i] == '.')
-                {
-                    state = 1;
-                    precision = 0;
-                }
-                else
-                {
-                    state = 2;
-                }
-                break;
+	if (format[curr_i] != '.')
+		return (precision);
 
-            case 1:  // parsing precision
-                if (is_digit(format[curr_i]))
-                {
-                    precision = precision * 10 + (format[curr_i] - '0');
-                }
-                else if (format[curr_i] == '*')
-                {
-                    precision = va_arg(list, int);
-                    state = 2;
-                }
-                else
-                {
-                    state = 2;
-                    *i = curr_i - 1;
-                    return precision;
-                }
-                break;
+	precision = 0;
 
-            case 2:  // done parsing
-                *i = curr_i - 1;
-                return precision;
-        }
+	for (curr_i += 1; format[curr_i] != '\0'; curr_i++)
+	{
+		if (is_digit(format[curr_i]))
+		{
+			precision *= 10;
+			precision += format[curr_i] - '0';
+		}
+		else if (format[curr_i] == '*')
+		{
+			curr_i++;
+			precision = va_arg(list, int);
+			break;
+		}
+		else
+			break;
+	}
 
-        curr_i++;
-    }
+	*i = curr_i - 1;
 
-    *i = curr_i - 1;
-    return precision;
+	return (precision);
 }
